56efdb6dde34334e8c3f7411d40e20b89fa63271
Agatha Monfredini <agathamonfredini14@gmail.com>
2025-06-26 20:13:10 -0300

    Apaga pasta ui

--- studio-ghibli-api/src/main/java/br/com/agatha/monfredini/studio_ghibli_api/ui/Constantes.kt
+++
@@ -1,8 +0,0 @@
-package br.com.agatha.monfredini.studioghibli.ui
-
-const val TAG_LISTA_FILMES = "ListaDeFilmes"
-const val TAG_DETALHES = "Detalhes"
-const val CHAVE_FILME = "FILME"
-const val CHAVE_PERSONAGEM = "PERSONAGEM"
-const val MENSAGEM_FILME_INVALIDO = "Filme Inválido"
-const val MENSAGEM_PERSONAGEM_INVALIDO = "Filme Inválido"
