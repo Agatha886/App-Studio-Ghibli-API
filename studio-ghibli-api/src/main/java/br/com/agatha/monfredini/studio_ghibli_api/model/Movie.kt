0b6691b176e978ce9767337babb8f44cb883ade8
Agatha Monfredini <agathamonfredini14@gmail.com>
2025-07-11 19:45:43 -0300

    Create imageUrl

--- studio-ghibli-api/src/main/java/br/com/agatha/monfredini/studio_ghibli_api/model/Movie.kt
+++ studio-ghibli-api/src/main/java/br/com/agatha/monfredini/studio_ghibli_api/model/Movie.kt
@@ -9,6 +9,7 @@ data class Movie(
     @SerializedName("release_date") val releaseDate: String,
     val people: List<String>,
     val species: List<String>,
+    var imageUrl: String = "https://www.ghibli.jp/img/totoro.png"
 ) : Serializable {
-    fun getImage(): GhibliImage = GhibliImage(title)
+    fun getImageDrawable(): GhibliImage = GhibliImage(title)
 }
